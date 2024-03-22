79ae74503050e0f7f3b73f7340005ba28e7d4545
Marek Mička <xmicka13@vutbr.cz>
2024-03-22 08:32:42 +0100

    chore: update

--- VendavoTest/Question1.cs
+++ VendavoTest/Question1.cs
@@ -2,6 +2,7 @@
 
 public static class Question1
 {
+    // I don't fully remember the task 1. I'm not exactly sure about possible inputs;
     public static int Solution(int[] A)
     {
         var isOneZero = false;
@@ -20,5 +21,41 @@ public static class Question1
         }
 
         return isOneZero ? 2 : A.Distinct().Count();
+    }
+
+    // possibly this - as I said I'm not sure what was the task exactly
+    public static int SolutionAlt(int[] A)
+    {
+        var isOneZero = false;
+        long total = 1;
+
+        for (var i = 0; i < A.Length; i++)
+        {
+            if (A[i] != 0)
+            {
+                total *= A[i];
+                continue;
+            }
+
+            if (isOneZero)
+            {
+                return 1;
+            }
+
+            isOneZero = true;
+        }
+
+        if (isOneZero)
+        {
+            return 2;
+        }
+
+        var hashset = new HashSet<long>();
+        for (var i = 0; i < A.Length; i++)
+        {
+            hashset.Add(total / A[i]);
+        }
+
+        return hashset.Count;
     }
 }
